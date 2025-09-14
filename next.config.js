9aabf8a3cf36d4ea9e9dc13287fa13ae00ff8f57
frostmag155 <maximtroshkin12@gmail.com>
2025-09-14 18:12:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
   output: 'export', // Важно для GitHub Pages!
   trailingSlash: true,
   images: {
